52a1600a68539fe36718f3677720dc1337ca5325
Rob_dev <72495599+Le-Caignec@users.noreply.github.com>
2023-06-01 10:35:04 +0200

    Update vite.config.ts
    Co-authored-by: pjt <26487010+PierreJeanjacquot@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 8080, // you can replace this port with any port
-  },
 });
