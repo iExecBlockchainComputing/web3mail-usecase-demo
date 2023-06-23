f937f9ffbaaa608bc27e2b20dc27422eac665c50
SeddikBellamine <bellamine.seddik@gmail.com>
2023-06-23 15:40:38 +0200

    add ui-kit chip instead of material ui chip

--- src/components/ProtectedDataCard.tsx
+++ src/components/ProtectedDataCard.tsx
@@ -1,9 +1,10 @@
-import { useNavigate } from 'react-router-dom';
-import './ProtectedDataCard.css';
-import { Box, Card, CardContent, Chip, Divider } from '@mui/material';
 import { DataSchema } from '@iexec/dataprotector';
-import { isKeyInDataSchema } from '../utils/utils';
+import Chip from "@iexec/react-ui-kit/components/Chip";
+import { Box, Card, CardContent, Divider } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import { CONSENT, PROTECTED_DATA } from '../config/path';
+import { isKeyInDataSchema } from '../utils/utils';
+import './ProtectedDataCard.css';
 
 export interface ProtectedDataProps {
   id: string;
@@ -18,12 +19,12 @@ export default function ProtectedDataCard(props: ProtectedDataProps) {
       sx={{ minWidth: 260, cursor: 'pointer' }}
       onClick={() => navigate(`/${PROTECTED_DATA}/${CONSENT}/${props.id}`)}
     >
-      <CardContent id="cardContent">
+      <CardContent className='cardContent'>
         <Box
           sx={{ height: '60px', display: 'flex', justifyContent: 'flex-end' }}
         >
           <Chip
-            id="chipLabel"
+            className="chipLabel"
             label={
               (isKeyInDataSchema(props.schema, 'email') && 'Email') ||
               (isKeyInDataSchema(props.schema, 'file') && 'File') ||
@@ -42,7 +43,7 @@ export default function ProtectedDataCard(props: ProtectedDataProps) {
           }}
         >
           <h5>{props.title}</h5>
-          <Chip id="chipDate" label={'Date'} size="small" />
+          <Chip className='chipDate' label={'Date'} size="small" />
         </Box>
       </CardContent>
     </Card>
