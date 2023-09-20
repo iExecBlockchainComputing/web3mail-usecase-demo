b0d5f95ef79c3611c7ae5310eaf935b3fb95324a
SeddikBellamine <bellamine.seddik@gmail.com>
2023-09-20 16:30:28 +0200

    add cypress config & scripts

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://127.0.0.1:5173',
+    video: true,
+    screenshotOnRunFailure: true,
+  },
+});
