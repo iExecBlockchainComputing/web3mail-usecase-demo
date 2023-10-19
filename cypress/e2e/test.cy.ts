1f06418a755972115c63646df7e18c5321933b6b
Dmialon <114732695+Dmialon@users.noreply.github.com>
2023-10-19 10:19:16 +0200

    supress test.cy.ts

--- cypress/e2e/test.cy.ts
+++
@@ -1,6 +0,0 @@
-beforeEach(() => {
-  cy.visit('/', { selectedWallet: 'metamask' });
-  cy.get('body').as('body');
-  cy.on('uncaught:exception', (e, runnable) => {
-    console.log('uncaught:exception', e);
-    return false;
