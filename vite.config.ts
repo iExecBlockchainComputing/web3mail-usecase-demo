3a2c3cf948974c4b1033f0cacc7be005b0e95162
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2023-11-08 15:57:31 +0100

    Be able to run bundle visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
+// import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // visualizer(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
