34b1d243c3a4714b13475724fc6f05cc644f70c2
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2023-11-09 00:06:30 +0100

    toast z-index higher than modal (1300)

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      'fixed top-[4.4rem] z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:right-0 sm:flex-col md:max-w-[420px]',
+      'fixed top-[4.4rem] z-[1400] flex max-h-screen w-full flex-col-reverse p-4 sm:right-0 sm:flex-col md:max-w-[420px]',
       className
     )}
     {...props}
