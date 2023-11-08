967a84d3f002374c1fbd0ffda4b20d9efd604bba
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2023-11-09 00:25:16 +0100

    Simplify success / error toasters when revoking an access

--- src/features/myProtectedData/OneProtectedData/AuthorizedUsersList.tsx
+++ src/features/myProtectedData/OneProtectedData/AuthorizedUsersList.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Avatar, List, Snackbar, Alert } from '@mui/material';
+import { Avatar } from '@mui/material';
+import { Loader } from 'react-feather';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useRevokeOneAccessMutation } from '@/app/appSlice.ts';
 import { Button } from '@/components/ui/button.tsx';
+import { toast } from '@/components/ui/use-toast.ts';
 import './AuthorizedUsersList.css';
 
 interface AuthorizedUsersListProps {
@@ -27,47 +28,26 @@ export default function AuthorizedUsersList(props: AuthorizedUsersListProps) {
   const users = authorizedUsers.map((user) => ({ id: user }));
 
   //query RTK API as mutation hook
-  const [revokeOneAccess] = useRevokeOneAccessMutation();
+  const [revokeOneAccess, result] = useRevokeOneAccessMutation();
 
-  // Snackbar notifications
-  const [isSnackbarVisible, setSnackbarVisible] = useState(false);
-  const [isErrorSnackbarVisible, setErrorSnackbarVisible] = useState(false);
-
-  const handleDelete = (value: string) => async () => {
-    if (ProtectedDataId !== undefined) {
-      revokeOneAccess({
-        protectedData: ProtectedDataId,
-        authorizedUser: value,
-      })
-        .unwrap()
-        .then(() => {
-          setSnackbarVisible(true);
-        })
-        .catch(() => {
-          setErrorSnackbarVisible(true);
+  function handleRevoke(value: string) {
+    revokeOneAccess({
+      protectedData: ProtectedDataId,
+      authorizedUser: value,
+    })
+      .unwrap()
+      .then(() => {
+        toast({
+          title: 'The granted access has been successfully revoked!',
         });
-    }
-  };
-
-  const handleCloseSnackbar = (
-    _: React.SyntheticEvent | Event,
-    reason?: string
-  ) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    setSnackbarVisible(false);
-  };
-
-  const handleCloseErrorSnackbar = (
-    _: React.SyntheticEvent | Event,
-    reason?: string
-  ) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    setErrorSnackbarVisible(false);
-  };
+      })
+      .catch((err) => {
+        toast({
+          variant: 'danger',
+          title: err || 'Failed to revoke access.',
+        });
+      });
+  }
 
   const columns: GridColDef[] = [
     {
@@ -92,9 +72,13 @@ export default function AuthorizedUsersList(props: AuthorizedUsersListProps) {
           size="sm"
           variant="secondary"
           className="pl-4"
-          onClick={() => handleDelete(params.value)}
+          onClick={() => handleRevoke(params.value)}
         >
-          <DeleteIcon fontSize="small" aria-label="delete" />
+          {result.isLoading ? (
+            <Loader className="animate-spin-slow" size="16" />
+          ) : (
+            <DeleteIcon fontSize="small" aria-label="delete" />
+          )}
           <span className="pl-2">Revoke access</span>
         </Button>
       ),
@@ -102,55 +86,21 @@ export default function AuthorizedUsersList(props: AuthorizedUsersListProps) {
   ];
 
   return (
-    <>
-      <List>
-        <DataGrid
-          disableColumnMenu
-          autoHeight
-          rows={users}
-          columns={columns}
-          paginationMode="server"
-          paginationModel={paginationModel}
-          pageSizeOptions={[pageSize]}
-          rowCount={count}
-          onPaginationModelChange={({ page: newPage }) =>
-            onPageChanged(newPage)
-          }
-          sx={{ border: 'none' }}
-          disableRowSelectionOnClick={true}
-          className="authorized-users-list"
-        />
-
-        <Snackbar
-          open={isSnackbarVisible}
-          autoHideDuration={6000}
-          onClose={handleCloseSnackbar}
-          anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-        >
-          <Alert
-            onClose={handleCloseSnackbar}
-            severity="success"
-            sx={{ width: '100%' }}
-          >
-            The granted access has been successfully revoked!
-          </Alert>
-        </Snackbar>
-
-        <Snackbar
-          open={isErrorSnackbarVisible}
-          autoHideDuration={6000}
-          onClose={handleCloseErrorSnackbar}
-          anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-        >
-          <Alert
-            onClose={handleCloseErrorSnackbar}
-            severity="error"
-            sx={{ width: '100%' }}
-          >
-            Failed to revoke access
-          </Alert>
-        </Snackbar>
-      </List>
-    </>
+    <div className="mt-6">
+      <DataGrid
+        disableColumnMenu
+        autoHeight
+        rows={users}
+        columns={columns}
+        paginationMode="server"
+        paginationModel={paginationModel}
+        pageSizeOptions={[pageSize]}
+        rowCount={count}
+        onPaginationModelChange={({ page: newPage }) => onPageChanged(newPage)}
+        sx={{ border: 'none' }}
+        disableRowSelectionOnClick={true}
+        className="authorized-users-list"
+      />
+    </div>
   );
 }
