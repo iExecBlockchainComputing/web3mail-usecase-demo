f40724b9ca32a59f81f718704f71566792fe6b4d
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2023-11-12 10:14:07 +0100

    Quick trick to be able to copy NavBar address

--- src/components/NavBar/AddressChip.tsx
+++ src/components/NavBar/AddressChip.tsx
@@ -1,4 +1,5 @@
 import { createIcon } from '@download/blockies';
+import { useToast } from '@/components/ui/use-toast.ts';
 
 type AddressForNavBarProps = {
   address: string;
@@ -6,6 +7,8 @@ type AddressForNavBarProps = {
 
 export default function AddressChip(props: AddressForNavBarProps) {
   const { address } = props;
+
+  const { toast } = useToast();
 
   const addressIcon = createIcon({
     seed: address.toLowerCase(),
@@ -18,11 +21,22 @@ export default function AddressChip(props: AddressForNavBarProps) {
   return (
     <div className="flex items-center rounded-md bg-grey-800 px-3 py-2">
       <div className="text-sm font-medium text-primary">{displayAddress}</div>
-      <img
-        src={addressIcon}
-        alt="Generated address icon"
-        className="ml-2 h-4 w-4 rounded-full"
-      />
+      <button
+        className="bg-grey-800"
+        onClick={() => {
+          navigator.clipboard.writeText(address);
+          toast({
+            title: 'Address copied!',
+            duration: 1200,
+          });
+        }}
+      >
+        <img
+          src={addressIcon}
+          alt="Generated address icon"
+          className="ml-2 h-4 w-4 rounded-full"
+        />
+      </button>
     </div>
   );
 }
