fa9e9d3c163f0bd3213aa0c03faacd14aaae94d2
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2023-11-30 14:11:55 +0100

    kit: Don't shrink profile icon + some padding to make click easier

--- src/components/NavBar/AddressChip.tsx
+++ src/components/NavBar/AddressChip.tsx
@@ -19,10 +19,10 @@ export default function AddressChip(props: AddressForNavBarProps) {
   )}`;
 
   return (
-    <div className="flex items-center rounded-md bg-grey-800 px-3 py-2">
+    <div className="flex shrink-0 items-center rounded-md bg-grey-800 px-3 py-2">
       <div className="text-sm font-medium text-primary">{displayAddress}</div>
       <button
-        className="bg-grey-800"
+        className="-my-0.5 -mr-0.5 ml-1.5 shrink-0 bg-grey-800 px-0.5 py-0.5"
         onClick={() => {
           navigator.clipboard.writeText(address);
           toast({
@@ -34,7 +34,7 @@ export default function AddressChip(props: AddressForNavBarProps) {
         <img
           src={addressIcon}
           alt="Generated address icon"
-          className="ml-2 h-4 w-4 rounded-full"
+          className="h-4 w-4 rounded-full"
         />
       </button>
     </div>
