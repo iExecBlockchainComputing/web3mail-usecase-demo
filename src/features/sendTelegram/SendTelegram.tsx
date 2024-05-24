c332c60c142b2ccbca37b9c7498dd489c45cb5ad
Arthur YVARS <aodarty23@gmail.com>
2024-05-24 09:35:50 +0200

    add telegram tab to navbar

---
+++ src/features/sendTelegram/SendTelegram.tsx
@@ -0,0 +1,13 @@
+import { SEND_TELEGRAM } from '@/config/path.ts';
+import './SendTelegram.css';
+
+export default function SendTelegram() {
+
+  return (
+    <>
+      <div>
+        SendTelegram ${SEND_TELEGRAM}
+      </div>
+    </>
+  );
+}
