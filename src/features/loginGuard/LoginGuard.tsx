a5e19d4a701df954991d6d44957ca31fe6d54d24
ErwanDecoster <wanerd2@gmail.com>
2024-09-16 11:15:33 +0200

    fix bad condition, code cleaning

--- src/features/loginGuard/LoginGuard.tsx
+++ src/features/loginGuard/LoginGuard.tsx
@@ -1,12 +1,12 @@
 import { useUserStore } from '@/stores/user.store.ts';
 import { FC, ReactNode } from 'react';
 import { AlertCircle } from 'react-feather';
+import { useSwitchChain, useAccount } from 'wagmi';
 import { Button } from '@/components/ui/button.tsx';
-import { useAccount, useSwitchChain } from 'wagmi';
 
 const LoginGuard: FC<{ children: ReactNode }> = ({ children }) => {
-  const { chains, error, isPending, switchChain } = useSwitchChain();
   const { isInitialized, isConnected } = useUserStore();
+  const { chains, error, isPending, switchChain } = useSwitchChain();
   const { chain } = useAccount();
 
   return (
@@ -16,18 +16,17 @@ const LoginGuard: FC<{ children: ReactNode }> = ({ children }) => {
       {isInitialized && !isConnected && (
         <p className="text-center text-lg">Please login with your wallet.</p>
       )}
-
-      {isInitialized && isConnected && chain?.id !== 134 && (
-        <div className="mx-auto my-12">
+      {isInitialized && isConnected && chain && chain.id !== 134 && (
+        <div className="mx-auto my-12 ml-12">
           <p>Oops, you're on the wrong network</p>
           <p>Click on the following button to switch to the right network</p>
           <Button
-            disabled={isPending || chain?.id === chains[0]?.id}
+            disabled={!isPending || chain?.id === chains[0]?.id}
             key={chains[0]?.id}
             onClick={() => switchChain({ chainId: chains[0]?.id })}
             className="mt-4"
           >
-            Switch to {chains?.[0]?.name}
+            Switch to {chains[0].name}
             {isPending && ' (switching)'}
           </Button>
           {error && (
